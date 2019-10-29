d4bdc0ec711f5bc568e2627929bc9e9546621a59
Hollie Putnam <hollieputnam@gmail.com>
2019-10-29 04:11:26 -0400

    updating env data

--- RAnalysis/Scripts/Surface_Area.R
+++ RAnalysis/Scripts/Surface_Area.R
@@ -24,6 +24,9 @@ plot(surface.area.cm2~delta.mass.g, data=Stnds)
 stnd.curve$coefficients
 
 fecundity <- subset(Data, Sample.Type=="Fecundity")
+fecundity$delta.mass.g <- fecundity$waxedmass.g-fecundity$mass.g
+fecundity$SA.cm2 <- stnd.curve$coefficients[2] * fecundity$delta.mass.g + stnd.curve$coefficients[1]
+
 
 E5.Data <- subset(Data, Sample.Type=="E5")
 E5.Data$delta.mass.g <- E5.Data$waxedmass.g-E5.Data$mass.g
